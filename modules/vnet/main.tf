da40754270bb6b7f7e6dfa93f27d70495a8d9467
amaravasu123 <133029972+amaravasu123@users.noreply.github.com>
2024-09-25 12:51:24 +0530

    added vnet configuration

---
+++ modules/vnet/main.tf
@@ -0,0 +1,44 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.subnet_address_prefixes
+
+  delegation {
+    name = "aks"
+    service_delegation {
+      name = "Microsoft.ContainerService/managedClusters"
+    }
+  }
+
+  security_group = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "Allow-HTTPS-Inbound"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
