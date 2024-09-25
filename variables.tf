04e6d590dedcc1d6bb00621b6ff58000f0bae167
amaravasu123 <133029972+amaravasu123@users.noreply.github.com>
2024-09-25 13:02:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -26,4 +26,34 @@ variable "subnet_name" {
 }
 
 variable "subnet_address_prefixes" {
- 
+  description = "Address prefixes for the Subnet"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "nsg_name" {
+  description = "Network Security Group name"
+  type        = string
+}
+
+variable "aks_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+}
+
+variable "dns_prefix" {
+  description = "DNS Prefix for the AKS cluster"
+  type        = string
+}
+
+variable "node_count" {
+  description = "Number of nodes in the AKS cluster"
+  type        = number
+  default     = 3
+}
+
+variable "vm_size" {
+  description = "Size of the nodes in the AKS cluster"
+  type        = string
+  default     = "Standard_DS2_v2"
+}
